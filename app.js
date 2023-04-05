b327d58fdfa8faf78e5f1af8967c447796f92c24
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-04-05 13:25:59 +0600

    Add listitems

--- app.js
+++ app.js
@@ -169,3 +169,23 @@ function setupItems() {
     }
 }
 
+function createListItem(id, value) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+      <!-- edit btn -->
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <!-- delete btn -->
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+  `;
+  
+}
